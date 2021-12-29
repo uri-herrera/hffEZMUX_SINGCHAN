75c914f5f7ba8895d064c1c4d35a4bca61fe40dd
urihe <uri.herrera08@gmail.com>
2021-12-29 14:21:44 -0800

    Added Sketch

--- libraryTest.ino
+++ libraryTest.ino
@@ -5,35 +5,56 @@
 #define TCA2 0x77
 
 TSL2591 test = TSL2591();
-boolean status;
 void setup() {
   Serial.begin(9600);
   Serial.println("TESTING");
-  //status = test.begin();
-  //Serial.println(status);
 }
    
 void loop() {
 
-  test.simpleRead(1,7);
+//  test.simpleRead(1,7);
+//  Serial.println("TSL Sensor #1");
+//  Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
+//  Serial.print(F("Luminosity: "));
+//  Serial.println(test.sro.lum, DEC);
+//  
+//  test.simpleRead(1,6);
+//  Serial.println("TSL Sensor #2");
+//  Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
+//  Serial.print(F("Luminosity: "));
+//  Serial.println(test.sro.lum, DEC);
+//  
+//  test.simpleRead(2,7);
+//  Serial.println("TSL Sensor #3");
+//  Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
+//  Serial.print(F("Luminosity: "));
+//  Serial.println(test.sro.lum, DEC);
+//  
+//  test.simpleRead(2,6);
+//  Serial.println("TSL Sensor #4");
+//  Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
+//  Serial.print(F("Luminosity: "));
+//  Serial.println(test.sro.lum, DEC);
+//  Serial.println();
+  test.simpleRead(2,3);
   Serial.println("TSL Sensor #1");
   Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
   Serial.print(F("Luminosity: "));
   Serial.println(test.sro.lum, DEC);
-  
-  test.simpleRead(1,6);
+
+  test.simpleRead(2,2);
   Serial.println("TSL Sensor #2");
   Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
   Serial.print(F("Luminosity: "));
   Serial.println(test.sro.lum, DEC);
-  
-  test.simpleRead(2,7);
+
+  test.simpleRead(4,3);
   Serial.println("TSL Sensor #3");
   Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
   Serial.print(F("Luminosity: "));
   Serial.println(test.sro.lum, DEC);
-  
-  test.simpleRead(2,6);
+
+  test.simpleRead(4,2);
   Serial.println("TSL Sensor #4");
   Serial.print(F("[ ")); Serial.print(test.sro.ms); Serial.print(F(" ms ] "));
   Serial.print(F("Luminosity: "));
